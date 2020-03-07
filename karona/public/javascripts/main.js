f964fb8e9364db896c1dfb652a056ecf3c39d288
malfusion <judenaveenraj@gmail.com>
2020-03-07 00:04:42 -0500

    Added rounding

--- karona/public/javascripts/main.js
+++ karona/public/javascripts/main.js
@@ -1,17 +1,17 @@
 window.addEventListener('deviceorientation', function(event) {
-    document.getElementById('beta').innerHTML = event.beta;
-    document.getElementById('gamma').innerHTML = event.gamma;
-    document.getElementById('alpha').innerHTML = event.alpha;
+    document.getElementById('beta').innerHTML = event.beta.toFixed(3);
+    document.getElementById('gamma').innerHTML = event.gamma.toFixed(3);
+    document.getElementById('alpha').innerHTML = event.alpha.toFixed(3);
  });
 
  window.addEventListener('devicemotion', function(event) {
-    document.getElementById('acceleration-x').innerHTML = event.acceleration.x;
-    document.getElementById('acceleration-y').innerHTML = event.acceleration.y;
-    document.getElementById('acceleration-z').innerHTML = event.acceleration.z;
+    document.getElementById('acceleration-x').innerHTML = event.acceleration.x.toFixed(3);
+    document.getElementById('acceleration-y').innerHTML = event.acceleration.y.toFixed(3);
+    document.getElementById('acceleration-z').innerHTML = event.acceleration.z.toFixed(3);
 
-    document.getElementById('rotation-rate-beta').innerHTML = event.rotationRate.beta;
-    document.getElementById('rotation-rate-gamma').innerHTML = event.rotationRate.gamma;
-    document.getElementById('rotation-rate-alpha').innerHTML = event.rotationRate.alpha;
+    document.getElementById('rotation-rate-beta').innerHTML = event.rotationRate.beta.toFixed(3);
+    document.getElementById('rotation-rate-gamma').innerHTML = event.rotationRate.gamma.toFixed(3);
+    document.getElementById('rotation-rate-alpha').innerHTML = event.rotationRate.alpha.toFixed(3);
 
-    document.getElementById('interval').innerHTML = event.interval;
+    document.getElementById('interval').innerHTML = event.interval.toFixed(3);
  });
