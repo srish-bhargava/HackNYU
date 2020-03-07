4b7d58c9a04215a9f4083e02f1d17257879f8b27
vidit23 <viditvineet@hotmail.com>
2020-03-07 01:21:26 -0500

    Post request

--- karona/routes/index.js
+++ karona/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var storingData = [];
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/addMovementData', function (req, res) {
+  storingData.push(JSON.parse(req.body.data).data);
+  console.log(storingData);
+  res.status(200).send("Done");
+});
+
 
 module.exports = router;
